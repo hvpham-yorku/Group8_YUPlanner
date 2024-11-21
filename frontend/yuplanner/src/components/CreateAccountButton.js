336eb0b3ec5cc8ffdbef7fabf25b0fb90b2d12e5
anushamasroor <anusham@my.yorku.ca>
2024-11-21 18:18:41 -0500

    Updated create account button
    started the code to connect the backend

--- frontend/yuplanner/src/components/CreateAccountButton.js
+++ frontend/yuplanner/src/components/CreateAccountButton.js
@@ -2,12 +2,22 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import SignupTextFields from './SignupTextField';
 
 export default function CreateAccountButton() {
   const navigate = useNavigate();
 
   const handleCreateAccountClick = () => {
-    navigate('/signup');
+    e.preventDefault()
+    const student={name,address}
+    console.log(student)
+    fetch("http://localhost:8080/student/add",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(student)
+
+    })
+    navigate('/login');
   };
 
   return (
